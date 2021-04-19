d1c3dfe408e24975341d23d28045e9bdec2bbb02
Grigor Georgiev <contact@grigorgeorgiev.com>
2021-04-19 14:51:17 +0300

    Update index.php

--- flexbox-css-in-20-min-traversy-media/index.php
+++ flexbox-css-in-20-min-traversy-media/index.php
@@ -1,1 +1,38 @@
+<style>
+  .container-1{
+    display: flex; 
+  }
 
+  .container-1 div{
+    border: 1px #ccc solid;
+    padding: 10px;
+  }
+  
+  .box-1{
+  
+  }
+  
+  .box-2{
+    
+  }
+  
+  .box-3{
+    
+  }
+  
+</style>
+
+<div class="container-1">
+  <div class="box-1">
+    <h3>Box One</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </div>
+  <div class="box-2">
+    <h3>Box Two</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </div>
+  <div class="box-3">
+    <h3>Box Three</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </div>
+</div>
